df62e94d1e3cf4946f2fa2e3eb0d03d42b9eeaed
Luke Knight <lukeknoot@gmail.com>
2019-10-21 00:44:55 +1100

    Refactor generation script to use new mapping

--- scripts/generate_all.ts
+++ scripts/generate_all.ts
@@ -1,25 +1,15 @@
 import * as Guidelime from '../src/guidelime';
 import * as Writer from '../src/writer';
+import * as ClassicWow from '../src/classicwow';
 
 import * as fs from 'fs-extra';
-
-const raceClassCombos = {
-  human: ['warrior', 'paladin', 'rogue', 'priest', 'warlock'],
-  dwarf: ['warrior', 'hunter', 'rogue', 'paladin', 'priest'],
-  nightelf: ['warrior', 'hunter', 'rogue', 'priest', 'druid'],
-  gnome: ['warrior', 'rogue', 'mage', 'warlock'],
-  orc: ['warrior', 'rogue', 'hunter', 'warlock', 'shaman'],
-  undead: ['warrior', 'rogue', 'priest', 'mage', 'warlock'],
-  tauren: ['warrior', 'hunter', 'druid', 'shaman'],
-  troll: ['warrior', 'hunter', 'rogue', 'shaman', 'priest', 'mage'],
-};
 
 console.log('Deleting old guides');
 
 fs.removeSync(__dirname + '/../src/tmp');
 
-Object.keys(raceClassCombos).forEach(wowRace => {
-  const classes = raceClassCombos[wowRace];
+Object.keys(ClassicWow.raceClassMapping).forEach(wowRace => {
+  const classes = ClassicWow.raceClassMapping[wowRace];
   classes.forEach(wowClass => {
     console.log('Generating Guidelime guide for ' + wowRace + ' ' + wowClass);
     const guide = Guidelime.generateGuide(wowRace, wowClass);
