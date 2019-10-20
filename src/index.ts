d42cf184ba5233f9067f57695aa2fc03c1bb6895
Jason <jason.dalmazzo@gmail.com>
2019-10-20 17:45:44 +1100

    Handle incorrect race class combinations

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,9 @@ import * as ClassicWow from './classicwow';
 import * as Writer from './writer';
 import * as Guidelime from './guidelime';
 
-if (process.argv.length < 4) {
-  console.log('Please provide race and class to generate Guidelime guide for.');
+if (process.argv.length !== 4) {
+  console.log('Incorrect usage. Arguments must be in the format: <race> <class>');
+  console.log('Example: yarn generateGuide human warrior');
 } else {
   const wowRace = process.argv[2].toLowerCase();
   const wowClass = process.argv[3].toLowerCase();
@@ -11,6 +12,8 @@ if (process.argv.length < 4) {
     console.log(`Class "${wowClass}" is invalid`);
   } else if (typeof ClassicWow.races[wowRace] === 'undefined') {
     console.log(`Race "${wowRace}" is invalid`);
+  } else if (!ClassicWow.raceClassMapping[wowRace].includes(wowClass)) {
+    console.log(`Race "${wowRace}" can not be a "${wowClass}"`);
   } else {
     console.log('Generating Guidelime guide for ' + wowRace + ' ' + wowClass);
     const guide = Guidelime.generateGuide(wowRace, wowClass);
