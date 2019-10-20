34575aa41ac0a76751fad989d639629d06d9b32f
Luke Knight <lukeknoot@gmail.com>
2019-10-20 16:21:54 +1100

    Add tests for step filtering
    Tests for #3.
    
    Includes jest installation as part of this.

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  roots: ['<rootDir>/src'],
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  testRegex: '/__tests__/[A-Za-z0-9]+\\.(test|spec)\\.[jt]sx?$',
+};
---
+++ src/__tests__/classicwow.test.ts
@@ -0,0 +1,23 @@
+import { getLevelingSteps } from '../classicwow';
+import * as fs from 'fs';
+
+const levelingData = JSON.parse(fs.readFileSync(__dirname + '/../resources/horde.json', 'utf-8'));
+
+test('Filtering removes non-specific class steps (regression #3)', () => {
+  const steps = getLevelingSteps('priest', 'troll', levelingData);
+  Object.keys(steps).forEach(levelBracket => {
+    expect(
+      steps[levelBracket].filter(step => {
+        step[1].includes('Tame');
+      })
+    ).toHaveLength(0);
+  });
+});
+
+test('Filtering keeps relevant class-specific steps (regression #3)', () => {
+  const steps = getLevelingSteps('hunter', 'troll', levelingData);
+  console.log();
+  expect(
+    steps[Object.keys(steps)[0]].filter(step => step[1].includes('Tame')).length
+  ).toBeGreaterThan(0);
+});
